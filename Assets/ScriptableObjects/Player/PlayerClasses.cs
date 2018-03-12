9574fa97e37df3d278d8b751520dd0b3d2757f05
DatBoiFox <edvardas.taip@gmail.com>
2018-03-12 19:18:30 +0200

    Simple Player Controlls

---
+++ Assets/ScriptableObjects/Player/PlayerClasses.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerClass", menuName = "PlayerClasses")]
+public class PlayerClasses : ScriptableObject {
+
+    public new string name;
+    public int health;
+    public int damage;
+    public int movementSpeed;
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public PlayerClasses playerClass;
+
+    private CharacterController controller;
+    public float gravityScale;
+    private Vector3 moveDir;
+    private Vector3 dashDir;
+    [SerializeField]
+    private bool isInvulnerable;
+
+    public float timer;
+
+    private void Start()
+    {
+        isInvulnerable = false;
+        moveDir = Vector3.zero;
+        dashDir = Vector3.zero;
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(dashTimer(timer));
+        }
+
+        if (!isInvulnerable)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal") * playerClass.movementSpeed, 0f, Input.GetAxis("Vertical") * playerClass.movementSpeed);
+            moveDir.y = moveDir.y + (Physics.gravity.y * gravityScale);
+            dashDir = moveDir;
+            controller.Move(moveDir * Time.deltaTime);
+        }else
+        {
+            Dash();
+        }
+        
+
+    }
+
+    IEnumerator dashTimer(float seconds)
+    {
+        isInvulnerable = true;
+        yield return new WaitForSeconds(seconds);
+        isInvulnerable = false;
+        
+    }
+
+    private void Dash()
+    {
+        controller.Move(dashDir * Time.deltaTime);
+    }
+
+}
